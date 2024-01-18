3fc3b9467814c02bc3c3cf089ccd86290091f108
anonymousdeveloper8922 <153376783+anonymousdeveloper8922@users.noreply.github.com>
2024-01-18 01:13:21 -0800

    Start SQLite database

---
+++ sqlite/sql.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE DATES (date NOT NULL, symptoms NOT NULL, notes NOT NULL);
+
+-- INSERT INTO dates (date, symptoms, notes)
+-- VALUES ("test2", "cramps", "hello2");
+
+SELECT * FROM dates;
+-- DELETE FROM dates WHERE date = "test";
